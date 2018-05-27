2b5f79f84978539b4e8e117776458e32f154c404
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-27 15:32:34 -0400

    Add Brotherhood model

--- api/brotherhoods/models.py
+++ api/brotherhoods/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Brotherhood(models.Model):
+    created = models.DateField()
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    # Create a timestamp when the brotherhood is added
+    registered = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('registered',)
