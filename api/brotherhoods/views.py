5595bbe2357047a4160f74394c5a2ef94ccccc43
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-27 15:54:06 -0400

    Add views

--- api/brotherhoods/views.py
+++ api/brotherhoods/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from brotherhoods.models import Brotherhood
+from brotherhoods.serializers import BrotherhoodSerializer
+from rest_framework import generics
 
-# Create your views here.
+class BrotherhoodList(generics.ListCreateAPIView):
+    queryset = Brotherhood.objects.all()
+    serializer_class = BrotherhoodSerializer
+
+class BrotherhoodDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Brotherhood.objects.all()
+    serializer_class = BrotherhoodSerializer
