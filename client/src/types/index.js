dab406e9a0b7c5338df80d44326d09de90b34ed6
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-28 13:45:32 -0400

    Add all types

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -1,5 +1,11 @@
 // Brotherhoods
+export const FETCH_BROTHERHOODS = 'fetch_brotherhood';
+export const FETCH_BROTHERHOOD = 'fetch_brotherhood';
 export const CREATE_BROTHERHOOD = 'create_brotherhood';
+export const EDIT_BROTHERHOOD = 'edit_brotherhood';
+export const DELETE_BROTHERHOOD = 'delete_brotherhood';
 
 // Alerts
+export const SUCCESS = 'success';
+export const WARNING = 'warning';
 export const ERROR = 'error';
