f917d4e37082c96f88c527e40fbfd4fcb6a741b2
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-28 15:28:46 -0400

    Add fetchBrotherhood action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,31 @@
 import axios from 'axios';
-import { CREATE_BROTHERHOOD, ERROR } from '../types';
+import {
+  CREATE_BROTHERHOOD,
+  ERROR,
+  FETCH_BROTHERHOOD,
+  FETCH_BROTHERHOODS
+} from '../types';
 
 const ROOT_URL = 'http://127.0.0.1:8000/brotherhoods/';
+
+export function fetchBrotherhoods() {
+  return function(dispatch) {
+    axios
+      .get(`${ROOT_URL}`)
+      .then(response => {
+        dispatch({
+          type: FETCH_BROTHERHOODS,
+          payload: response
+        });
+      })
+      .catch(error => {
+        dispatch({
+          type: ERROR,
+          payload: error.response.data.detail
+        });
+      });
+  };
+}
 
 export function createBrotherhood(data, callback) {
   return function(dispatch) {
