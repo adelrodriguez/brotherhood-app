6235da53e8b887761e84dcbc7a9c819e92ef5e8b
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-28 15:35:01 -0400

    Add cases to brotherhood reducer

--- client/src/reducers/brotherhoods.js
+++ client/src/reducers/brotherhoods.js
@@ -1,3 +1,23 @@
+import _ from 'lodash';
+import {
+  FETCH_BROTHERHOODS,
+  FETCH_BROTHERHOOD,
+  CREATE_BROTHERHOOD,
+  EDIT_BROTHERHOOD,
+  DELETE_BROTHERHOOD
+} from '../types';
+
 export default function(state = {}, action) {
-  return state;
+  switch (action.type) {
+    case FETCH_BROTHERHOODS:
+      return _.mapKeys(action.payload.data, 'id');
+    case FETCH_BROTHERHOOD:
+    case CREATE_BROTHERHOOD:
+    case EDIT_BROTHERHOOD:
+      return { ...state, [action.payload.data.id]: action.payload.data };
+    case DELETE_BROTHERHOOD:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
 }
