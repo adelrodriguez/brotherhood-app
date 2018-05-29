72e4a7ee482d53701aba59ce55c8e8fb7616577e
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-29 01:20:46 -0400

    Add perform_create to views

--- api/brotherhoods/views.py
+++ api/brotherhoods/views.py
@@ -6,6 +6,19 @@ class BrotherhoodList(generics.ListCreateAPIView):
     queryset = Brotherhood.objects.all()
     serializer_class = BrotherhoodSerializer
 
+    def perform_create(self, serializer):
+        queryset = Brotherhood.objects.all()
+        serializer.save(place=len(queryset))
+
 class BrotherhoodDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Brotherhood.objects.all()
     serializer_class = BrotherhoodSerializer
+
+    # def perform_update(self, serializer):
+    #     old_data = self.get_object()
+    #     prev_place = old_data.place
+    #     new_place = self.request.data['place']
+
+    #     queryset = Brotherhood.objects.filter(place__range=(prev_place, new_place))
+    #     print(len(queryset))
+    #     serializer.save()
