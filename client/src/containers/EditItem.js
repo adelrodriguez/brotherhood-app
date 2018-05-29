e0b7a733779909c10f020aafd1e1c25d7cb6250b
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-28 21:03:57 -0400

    Add EditItem component

---
+++ client/src/containers/EditItem.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+
+import { editBrotherhood, deleteBrotherhood } from '../actions';
+
+import Input from '../components/Input';
+import Button from '../components/Button';
+
+class EditItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: props.match.params.id
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  onSubmit(data) {
+    this.props.editBrotherhood(this.state.id, data, () => {
+      this.props.history.push('/hermandades');
+    });
+  }
+
+  handleDelete() {
+    this.props.deleteBrotherhood(this.state.id, () => {
+      this.props.history.push('/hermandades');
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <div>
+        <h1>Editar Hermandad</h1>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <Field
+            label="Nombre de la hermandad"
+            name="name"
+            type="text"
+            id="nameInput"
+            component={Input}
+          />
+          <Field
+            label="Email"
+            name="email"
+            type="email"
+            id="emailInput"
+            component={Input}
+          />
+          <Field
+            label="Fecha de creación"
+            name="created"
+            type="date"
+            id="createdInput"
+            component={Input}
+          />
+          <Button text="Eliminar" handleClick={this.handleDelete} />
+          <Button text="Guardar cambios" />
+          <Link to="/hermandades">
+            <Button text="Volver" />
+          </Link>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(data) {
+  const errors = {};
+  const { name, email, created } = data;
+
+  if (!name) {
+    errors.name = 'Debes colocar un nombre';
+  }
+
+  if (!email) {
+    errors.email = 'Debes colocar un email';
+  }
+
+  if (!created) {
+    errors.created = 'Debes colocar una fecha';
+  }
+
+  return errors;
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    initialValues: state.brotherhoods[ownProps.match.params.id]
+  };
+}
+
+export default compose(
+  connect(mapStateToProps, { editBrotherhood, deleteBrotherhood }),
+  reduxForm({
+    validate,
+    form: 'editItem',
+    enableReinitialize: true
+  })
+)(EditItem);
