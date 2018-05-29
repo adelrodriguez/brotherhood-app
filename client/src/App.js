7ec84876f90ef88c2a60bc554fd8e0bb955187b4
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-29 15:39:54 -0400

    Add Logout to App

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import Register from './containers/Register';
 import List from './containers/List';
 import EditItem from './containers/EditItem';
 import Login from './containers/Login';
+import Logout from './containers/Logout';
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
         <Route path="/hermandades/:id/edit" component={EditItem} />
         <Route path="/hermandades" component={List} />
         <Route exact path="/" component={Register} />
