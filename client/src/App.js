d746977b03ad23ba4f8cb6b43e63adf6c1c953e4
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-28 21:04:36 -0400

    Add List and EditItem to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-// Components
+// Containers
 import Register from './containers/Register';
+import List from './containers/List';
+import EditItem from './containers/EditItem';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        {/* <Route path="/login" component={Login} /> */}
+        <Route path="/hermandades/:id/edit" component={EditItem} />
+        <Route path="/hermandades" component={List} />
         <Route exact path="/" component={Register} />
       </Switch>
     );
