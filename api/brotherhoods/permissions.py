bc8437dafb0cd9013dc3632b8ec2eceee0e05145
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-29 16:57:24 -0400

    Fix permission

--- api/brotherhoods/permissions.py
+++ api/brotherhoods/permissions.py
@@ -4,6 +4,9 @@ class AllowWriteAny(permissions.BasePermission):
     """
     Allow unauthenticated POST requests
     """
-
     def has_permission(self, request, view):
-        return request.method == 'POST'
+        if request.method == 'POST':
+            return True
+        elif request.user and request.user.is_authenticated:
+            return True
+        return False
