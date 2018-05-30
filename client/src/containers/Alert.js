2c1bb244bd97463eec37cc65db66de8839024e56
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-30 02:09:27 -0400

    Create Alert component

---
+++ client/src/containers/Alert.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { clearAlert } from '../actions';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.clearAlert();
+  }
+
+  render() {
+    const { alerts } = this.props;
+
+    if (!alerts.message) { return null }
+
+    return (
+      <div className={`notification ${alerts.class}`}>
+        <button onClick={this.handleClick} className="delete" />
+        {alerts.message}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { alerts: state.alerts };
+}
+
+export default connect(mapStateToProps, { clearAlert })(Alert);
