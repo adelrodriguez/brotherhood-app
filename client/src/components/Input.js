32c8b15ceb196c9da042652373ee7bf863c60e46
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-29 23:17:43 -0400

    Add style to Input

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 const Input = field => {
+  const {
+    meta: { touched, error }
+  } = field;
+
+  let valid = touched ? error ? 'is-danger': 'is-success' : '';
+
   return (
-    <div>
-      <label htmlFor={field.id}>{field.label}</label>{' '}
-      <input
-        type={field.type}
-        name={field.name}
-        id={field.id}
-        placeholder={field.placeholder}
-        {...field.input}
-        {...field}
-      />
+    <div className="field">
+      <div className="control">
+        <label className="label" htmlFor={field.id}>
+          {field.label}
+        </label>{' '}
+        <input
+          className={`input ${valid}`}
+          type={field.type}
+          name={field.name}
+          id={field.id}
+          placeholder={field.placeholder}
+          {...field.input}
+          {...field}
+        />
+      </div>
+      <p class="help is-danger">{touched ? error : ''}</p>
     </div>
   );
 };
