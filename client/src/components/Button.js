28fe7e861c652ac22e6727b6464a8d620114ebbf
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-30 03:19:37 -0400

    Add disabled props to Button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Button = props => {
   return (
-    <button className={`button ${props.className}`} onClick={props.handleClick}>
+    <button
+      className={`button ${props.className}`}
+      onClick={props.handleClick}
+      disabled={props.disabled}
+    >
       {props.text}
     </button>
   );
