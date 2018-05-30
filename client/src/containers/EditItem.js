c0c6bc167bd6209952855f45024b5d7ae77048ed
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-30 00:34:36 -0400

    Set EditItem style

--- client/src/containers/EditItem.js
+++ client/src/containers/EditItem.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { reduxForm, Field } from 'redux-form';
-import { Link } from 'react-router-dom';
 
 import { editBrotherhood, deleteBrotherhood } from '../actions';
 
+import Header from './Header';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
@@ -27,54 +27,77 @@ class EditItem extends Component {
     });
   }
 
-  handleDelete() {
-    this.props.deleteBrotherhood(this.state.id, () => {
-      this.props.history.push('/hermandades');
-    });
+  handleDelete(e) {
+    e.preventDefault();
+
+    if (
+      window.confirm('¿Estás seguro de que quieres eliminar esta hermandad?')
+    ) {
+      this.props.deleteBrotherhood(this.state.id, () => {
+        this.props.history.push('/hermandades');
+      });
+    }
   }
 
   render() {
     const { handleSubmit } = this.props;
 
     return (
-      <div>
-        <h1>Editar Hermandad</h1>
-        <form onSubmit={handleSubmit(this.onSubmit)}>
-          <Field
-            label="Nombre de la hermandad"
-            name="name"
-            type="text"
-            id="nameInput"
-            component={Input}
-          />
-          <Field
-            label="Email"
-            name="email"
-            type="email"
-            id="emailInput"
-            component={Input}
-          />
-          <Field
-            label="Fecha de creación"
-            name="created"
-            type="date"
-            id="createdInput"
-            component={Input}
-          />
-          <Field
-            label="Lugar"
-            name="place"
-            min="1"
-            type="number"
-            id="placeInput"
-            component={Input}
-          />
-          <Button text="Guardar cambios" />
-        </form>
-        <Button text="Eliminar" handleClick={this.handleDelete} />{' '}
-        <Link to="/hermandades">
-          <Button text="Volver" />
-        </Link>
+      <div className="section">
+        <div className="container">
+          <Header />
+          <div className="columns is-centered">
+            <div className="column is-half">
+              <h1 className="title">Editar Hermandad</h1>
+              <form onSubmit={handleSubmit(this.onSubmit)}>
+                <Field
+                  label="Nombre de la hermandad"
+                  name="name"
+                  type="text"
+                  id="nameInput"
+                  component={Input}
+                />
+                <Field
+                  label="Email"
+                  name="email"
+                  type="email"
+                  id="emailInput"
+                  component={Input}
+                />
+                <Field
+                  label="Fecha de creación"
+                  name="created"
+                  type="date"
+                  id="createdInput"
+                  component={Input}
+                />
+                <Field
+                  label="Lugar"
+                  name="place"
+                  min="1"
+                  type="number"
+                  id="placeInput"
+                  component={Input}
+                />
+                <div className="field is-grouped">
+                  <p className="control">
+                    <Button
+                      className="is-success is-outlined"
+                      text="Guardar cambios"
+                    />
+                  </p>
+                  <p className="control">
+                    <Button
+                      className="is-danger"
+                      text="Eliminar"
+                      handleClick={this.handleDelete}
+                    />
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -101,6 +124,7 @@ function validate(data) {
 
 function mapStateToProps(state, ownProps) {
   return {
+    brotherhoods: state.brotherhoods,
     initialValues: state.brotherhoods[ownProps.match.params.id]
   };
 }
