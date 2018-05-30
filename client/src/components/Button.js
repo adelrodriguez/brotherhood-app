bdde3ceb882b1f37d58aff9b2905ea65658f6a48
Adel Rodríguez <adelrodriguezv@gmail.com>
2018-05-29 23:18:03 -0400

    Add style to Button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = props => {
   return (
-    <button onClick={props.handleClick}>
+    <button className={`button ${props.className}`} onClick={props.handleClick}>
       {props.text}
     </button>
   );
